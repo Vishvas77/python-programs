c0366e4171372bc8399f797025933f1b3e1840a8
Vishvas77 <polepakavishvas@gmail.com>
2026-07-15 20:28:21 +0530

    Introduction to Python Lab: Installation and Simple Output Display.

--- WEEK-1/details.py
+++ WEEK-1/details.py
@@ -1,4 +1,7 @@
+#Write a python program to print your Name, Roll Number, and Branch.
 a="RAM"
 b=107
 c="CSE"
-print(ab,c)
+print("Name:", a)
+print("Roll Number:", b)
+print("Branch:", c)
--- WEEK-1/types.py
+++ WEEK-1/types.py
@@ -1,6 +1,7 @@
-a=int(input("Enter Roll: "))
-b=float(input("Enter marks: "))
-c=input("Enter Name: ")
-print(a);
-print(b);
-print(c);
+#Write a python program to read integer, float & string values and display them on the screen
+a=int(input("Enter integer: "))
+b=float(input("Enter float: "))
+c=input("Enter string: ")
+print("Integer:", a)
+print("Float:", b)
+print("String:", c)
